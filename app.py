e6971578f97e0da40987dd0f612ae56fb023203d
muditrajsade <muditrajsade89@gmail.com>
2025-06-24 19:30:09 +0300

    changed port to 8000

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def FindYourCrop():
     return render_template('FindYourCrop.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # get port from env or default 5000
+    port = int(os.environ.get("PORT", 8000))  # get port from env or default 5000
     app.run(host="0.0.0.0", port=port, debug=True)
